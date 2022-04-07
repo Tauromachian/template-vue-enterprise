30703086551b703604cd6108c13c77a29e236748
Jose Garcia <jogarcia@tuta.io>
2022-04-07 09:16:37 -0400

    Add auth layout

---
+++ src/layouts/auth/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <auth-view />
+    <FlashMessage
+      :position="'left bottom'"
+      class="first-to-view"
+    />
+  </v-app>
+</template>
+
+<script>
+  import AuthView from '@/layouts/auth/View'
+  export default {
+    name: 'HomeLayout',
+
+    components: {
+      AuthView,
+    },
+  }
+</script>
