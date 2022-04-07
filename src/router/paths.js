e7ea0d6c92b1cf2a5c7a4c40f3f6381c48bbb72b
Jose Garcia <jogarcia@tuta.io>
2022-04-07 09:32:33 -0400

    Make basic paths

---
+++ src/router/paths.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    path: '/',
+    component: () => import('@/layouts/home/Index.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('@/views/home/Home.vue'),
+      },
+    ],
+  },
+  {
+    path: '/login',
+    component: () => import('@/layouts/auth/Index.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Login',
+        component: () => import('@/views/auth/Login.vue'),
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: () => import('@/views/auth/Register.vue'),
+      },
+
+      {
+        path: '/password-forgot',
+        name: 'PasswordForgot',
+        component: () => import('@/views/auth/PasswordForgot.vue'),
+      },
+      {
+        path: '/password-reset/:hash',
+        title: 'Reset Password',
+        name: 'ResetPassword',
+        component: () => import('@/views/auth/PasswordReset.vue'),
+      },
+    ],
+  },
+]
