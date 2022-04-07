3d4ca10bab182e3d2b86de1fe7a878792a1901e1
Jose Garcia <jogarcia@tuta.io>
2022-04-07 09:39:30 -0400

    Add 404 view

---
+++ src/views/shared/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>404</h1>
+    <p>404 - Página no encontrada</p>
+    <p>Lo sentimos, no hemos podido encontrar la página que buscas</p>
+    <router-link :to="{ name: 'Home' }"></router-link>
+  </section>
+</template>
+
+<script>
+export default {
+  name: '404',
+}
+</script>
