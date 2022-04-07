c1ab7f7aa380d994c499d935060a0436914f1ead
Jose Garcia <jogarcia@tuta.io>
2022-04-07 09:32:42 -0400

    Use separated paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import paths from './paths'
 
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/login',
-    component: () => import('@/layouts/auth/Index.vue'),
-    children: [
-      {
-        path: '',
-        name: 'Login',
-        component: () => import('@/views/auth/Login.vue'),
-      },
-      {
-        path: '/register',
-        name: 'Register',
-        component: () => import('@/views/auth/Register.vue'),
-      },
-
-      {
-        path: '/password-forgot',
-        name: 'PasswordForgot',
-        component: () => import('@/views/auth/PasswordForgot.vue'),
-      },
-      {
-        path: '/password-reset/:hash',
-        title: 'Reset Password',
-        name: 'ResetPassword',
-        component: () => import('@/views/auth/PasswordReset.vue'),
-      },
-    ],
-  },
-]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes: paths,
 })
 
 export default router
