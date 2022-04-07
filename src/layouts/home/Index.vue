a97ac29bc472490b620e878d1628e8cc6846e073
Jose Garcia <jogarcia@tuta.io>
2022-04-07 09:19:08 -0400

    Add basic home layout

---
+++ src/layouts/home/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <home-view />
+  </v-app>
+</template>
+
+<script>
+import HomeView from './View.vue'
+
+export default {
+  name: 'HomeLayout',
+  components: { HomeView },
+}
+</script>
