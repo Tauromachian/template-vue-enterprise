9f7744a6caa9cbb6151dbfd49a108212716ad8af
Jose Garcia <jogarcia@tuta.io>
2022-05-03 10:02:32 -0400

    Update FormDialog

--- src/components/app/FormDialog.vue
+++ src/components/app/FormDialog.vue
@@ -1,12 +1,6 @@
 <template>
-  <v-dialog
-    v-model="dialog"
-    :max-width="maxWidth"
-  >
-    <template
-      v-if="enableAddButton"
-      v-slot:activator="{ on, attrs }"
-    >
+  <v-dialog v-model="dialog" :max-width="maxWidth">
+    <template v-if="enableAddButton" v-slot:activator="{ on, attrs }">
       <v-btn
         color="primary"
         fab
@@ -28,24 +22,13 @@
       >
         <span>{{ title }}</span>
         <v-spacer />
-        <v-btn
-          icon
-          @click="dialog = false"
-        >
-          <v-icon color="white">
-            mdi-close
-          </v-icon>
+        <v-btn icon @click="dialog = false">
+          <v-icon color="white"> mdi-close </v-icon>
         </v-btn>
       </v-card-title>
-      <v-card-text
-        :class="{ 'no-padding': noPadding }"
-        class="pt-4"
-      >
+      <v-card-text :class="{ 'no-padding': noPadding }" class="pt-4">
         <v-container :class="{ 'no-padding': noPadding }">
-          <v-form
-            ref="form"
-            :disabled="disabled"
-          >
+          <v-form @submit.prevent="accept" ref="form" :disabled="disabled">
             <slot />
           </v-form>
         </v-container>
@@ -53,10 +36,10 @@
       <v-card-actions v-if="!hideActions">
         <slot name="actions">
           <v-spacer />
-          <base-form-action-buttons
+          <app-form-action-buttons
             :disabled="disabled"
             class="mr-2"
-            @accept-click="onClick"
+            @accept-click="accept"
             @cancel-click="cancelClick"
           />
         </slot>
@@ -66,85 +49,85 @@
 </template>
 
 <script>
-  export default {
-    name: 'FormDialog',
-    props: {
-      value: {
-        type: Boolean,
-        default: false,
-      },
-      title: {
-        type: String,
-        default: 'Formulario',
-      },
-      maxWidth: {
-        type: [Number, String],
-        default: '375px',
-      },
-      disabled: {
-        type: Boolean,
-        default: false,
-      },
-      hideActions: {
-        type: Boolean,
-        default: false,
-      },
-      noPadding: {
-        type: Boolean,
-        default: false,
-      },
-      enableAddButton: {
-        type: Boolean,
-        default: false,
-      },
-      icon: {
-        type: String,
-        default: 'mdi-plus',
-      },
+export default {
+  name: 'FormDialog',
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
     },
-    data () {
-      return {
-        dialogInternal: false,
-      }
+    title: {
+      type: String,
+      default: 'Formulario',
     },
-    computed: {
-      dialog: {
-        get () {
-          if (this.value) {
-            return this.value
-          } else {
-            return this.dialogInternal
-          }
-        },
-        set (val) {
-          this.dialogInternal = val
-          this.$emit('input', val)
-        },
-      },
-      folderTitleColor () {
-        return this.$vuetify.theme.themes.light.primary
-      },
+    maxWidth: {
+      type: [Number, String],
+      default: '375px',
     },
-    methods: {
-      validate () {
-        return this.$refs.form.validate()
-      },
-      reset () {
-        return this.$refs.form.reset()
-      },
-      onClick () {
-        if (!this.validate()) {
-          return
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    hideActions: {
+      type: Boolean,
+      default: false,
+    },
+    noPadding: {
+      type: Boolean,
+      default: false,
+    },
+    enableAddButton: {
+      type: Boolean,
+      default: false,
+    },
+    icon: {
+      type: String,
+      default: 'mdi-plus',
+    },
+  },
+  data() {
+    return {
+      dialogInternal: false,
+    }
+  },
+  computed: {
+    dialog: {
+      get() {
+        if (this.value) {
+          return this.value
+        } else {
+          return this.dialogInternal
         }
-        // this.dialog = false
-        this.$emit('accept-click')
       },
-      cancelClick () {
-        this.dialog = false
-        this.$emit('cancel-click')
+      set(val) {
+        this.dialogInternal = val
+        this.$emit('input', val)
       },
     },
-  }
+    folderTitleColor() {
+      return this.$vuetify.theme.themes.light.primary
+    },
+  },
+  methods: {
+    validate() {
+      return this.$refs.form.validate()
+    },
+    reset() {
+      return this.$refs.form.reset()
+    },
+    accept() {
+      if (!this.validate()) {
+        return
+      }
+      // this.dialog = false
+      this.$emit('accept-click')
+    },
+    cancelClick() {
+      this.dialog = false
+      this.$emit('cancel-click')
+    },
+  },
+}
 </script>
 
 <style>
