791bcc12a1419d13844a96b7d5b7a744ab645432
Jose Garcia <jogarcia@tuta.io>
2022-05-13 11:44:41 -0400

    Stop importing unnecesary variables

--- src/services/auth/auth.js
+++ src/services/auth/auth.js
@@ -1,10 +1,5 @@
 import axios from 'axios'
-import {
-  SERVER_DIR,
-  REGISTER_DIR,
-  HOST_DIR,
-  APPLICATION,
-} from '../../utils/constants'
+import { SERVER_DIR, HOST_DIR, APPLICATION } from '../../utils/constants'
 import qs from 'query-string'
 import { requestData, requestDataAuth } from '../requestHeadersHelper'
 
@@ -15,6 +10,12 @@ export async function login(data) {
   return response
 }
 
+export async function getUserInfo() {
+  const url = `${SERVER_DIR}user/info`
+  const response = await axios.get(url, requestData())
+  return response
+}
+
 // Auth register
 export async function register(data) {
   const url = `${SERVER_DIR}api/register`
@@ -22,29 +23,4 @@ export async function register(data) {
   data = qs.stringify(data)
   const response = await axios.post(url, data, requestDataAuth())
   return response
-}
-
-export async function getUserInfo() {
-  const url = `${SERVER_DIR}user/info`
-  const response = await axios.get(url, requestData())
-  return response
-}
-
-export function registerUser(user, cb) {
-  axios
-    .post(REGISTER_DIR, {
-      uid: user.email,
-      username: user.email,
-      mail: user.email,
-      name: user.name,
-      lastname: user.lastname,
-      password: user.password,
-      dni: user.dni,
-    })
-    .then(function (response) {
-      cb(null, response.data)
-    })
-    .catch((error) => {
-      cb(error)
-    })
 }
